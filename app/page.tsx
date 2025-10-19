f52f61c3183e35137e42029903b6eb75b067ca37
The-Guardians-Golden-Curtain <theguardians@thegoldencurtin.com>
2025-10-19 16:30:09 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import NotifyForm from "@/components/NotifyForm";
+import NotifyForm from "./components/NotifyForm";
 
 export default function Home() {
   return (
